53c15457d4d7fdf73dbe3c3181962a7dd0511a5a
cacko <alex@cacko.net>
2023-11-05 12:17:37 +0000

    lol digg

--- app/database/models/base.py
+++ app/database/models/base.py
@@ -21,8 +21,8 @@ def _default(self, obj):
     return getattr(obj.__class__, "to_json", _default.default)(obj)
 
 
-_default.default = JSONEncoder().default
-JSONEncoder.default = _default
+_default.default = PeeEncoder().default
+PeeEncoder.default = _default
 
 
 class DbModel(Model):
