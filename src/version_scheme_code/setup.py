3e6851f7d51c0a4a4e024f3449466517910bfed2
cacko <alex@cacko.net>
2024-06-05 18:37:12 +0100

    COMMIT ALL THE FILES!

---
+++ src/version_scheme_code/setup.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from setuptools import setup
+
+from setuptools_scm import ScmVersion
+
+
+def myversion_func(version: ScmVersion) -> str:
+    from setuptools_scm.version import guess_next_version
+
+    return version.format_next_version(guess_next_version, "{guessed}b{distance}")
+
+
+setup(use_scm_version={"version_scheme": myversion_func})
