2604410cbce0021c378ff14dc39f2af27f23a899
cacko <alex@cacko.net>
2025-08-27 23:56:59 +0100

    "update the diff to reflect the new job index state.  thanks @jeremyticel/jhnstawie@gjerstewheller.com 💈–…"

--- src/jobs/database/models/user.py
+++ src/jobs/database/models/user.py
@@ -1,17 +1,33 @@
-from itertools import groupby
-from yaml import Token
 from .base import DbModel
 from jobs.database import Database
 from jobs.database.fields import CleanCharField
-from jobs.routers.models import SkillResponse
-from humanfriendly.tables import format_robust_table
-
+from peewee import IntegrityError
 
 class User(DbModel):
     email = CleanCharField(unique=True)
     uuid = CleanCharField(unique=True)
-    name=CleanCharField(null=True)
+    name = CleanCharField(null=True)
+
+    @classmethod
+    def get_or_create(cls, **kwargs) -> tuple["User", bool]:
+        defaults = kwargs.pop("defaults", {})
+        query = cls.select()
+        query = query.where((User.uuid == kwargs.get("uuid")) | (User.email == kwargs.get("email")))
+
+        try:
+            return query.get(), False
+        except cls.DoesNotExist:
+            try:
+                if defaults:
+                    kwargs.update(defaults)
+                with cls._meta.database.atomic():
+                    return cls.create(**kwargs), True
+            except IntegrityError as exc:
+                try:
+                    return query.get(), False
+                except cls.DoesNotExist:
+                    raise exc
 
     class Meta:
         database = Database.db
-        table_name = 'jobs_users'
+        table_name = "jobs_users"
