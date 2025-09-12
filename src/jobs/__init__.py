bb9fcb6f822fbaf1f64f0e007ceaa4c9a7ef22e8
cacko <alex@cacko.net>
2025-09-12 07:48:32 +0100

    "add job log level to diff diff output. closes  [ci skip] 💯–—‘’“” →↓─└←↜↝"

--- src/jobs/__init__.py
+++ src/jobs/__init__.py
@@ -1,10 +1,9 @@
 __name__ = "jobs"
 
-from datetime import datetime, timezone
 import corelog
+import logging
 import os
-
-from jobs.firebase.db import UpdatesDb
 
 
 corelog.register(os.environ.get("JOBS_LOG_LEVEL", "INFO"))
+logging.getLogger("peewee").setLevel(os.environ.get("JOBS_LOG_LEVEL", "INFO"))
